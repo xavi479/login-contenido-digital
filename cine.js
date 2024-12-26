dace9d1bcf07f4d3f80feb72816d788c527838d8
Oscar Lopez <xavilopez581@gmail.com>
2024-12-26 16:24:16 -0500

    registe y login actualizado

--- cine.js
+++ cine.js
@@ -1,12 +1,28 @@
+
 function toggleRegisterForm() {
-  var registerForm = document.getElementById('registerForm');
-  var loginForm = document.getElementById('loginForm');
+  let registerForm = document.getElementById('registerForm');
+  let loginForm = document.querySelector('.formulary');
   
   if (registerForm.style.display === 'none') {
-      registerForm.style.display = 'block';
-      loginForm.style.display = 'none';
+    registerForm.style.display = 'block';
+    loginForm.style.display = 'none';
   } else {
-      registerForm.style.display = 'none';
-      loginForm.style.display = 'block';
+    registerForm.style.display = 'none';
+    loginForm.style.display = 'block';
   }
 }
+
+function register(event) {
+  event.preventDefault();
+  let username = document.querySelector('#registerForm input[name="username"]').value;
+  let password = document.querySelector('#registerForm input[name="password"]').value;
+  let email = document.querySelector('#registerForm input[name="email"]').value;
+
+  // Aquí puedes agregar la lógica para registrar al usuario
+  alert('Registration successful');
+  
+  // Ocultar el formulario de registro y mostrar el formulario de login
+  toggleRegisterForm();
+}
+
+document.querySelector('#registerForm form').addEventListener('submit', register);
